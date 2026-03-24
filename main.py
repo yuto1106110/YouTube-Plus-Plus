b56df9c00cb2f9bd2586b05c817b5d959409a1e3
yuto_110 <yuto.oio5.2011@gmail.com>
2026-03-24 18:52:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def getSearchData(q, page):
             "id": data_dict["videoId"] if 'videoId' in data_dict else failed,
             "authorId": data_dict["authorId"] if 'authorId' in data_dict else failed,
             "author": data_dict["author"] if 'author' in data_dict else failed,
-            "published": data_dict.get("publishedText", "") or formatPublished(data_dict.get("published", 0)),
+            "published": formatPublished(data_dict.get("published", 0)) if data_dict.get("published") else "",
             "length": str(datetime.timedelta(seconds=data_dict.get("lengthSeconds", 0))),
             "view_count_text": formatViewCount(data_dict.get("viewCount", 0)),
           }
